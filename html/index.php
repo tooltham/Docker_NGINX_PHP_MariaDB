30f9474f91eee312736b4a61cf18efd1e9dbe00b
Apirak Tooltham <apirak@npu.ac.th>
2023-08-28 19:58:45 +0700

    add initial code

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>My first PHP & HTML page</title>
+</head>
+
+<body>
+    <h1>My first PHP & HTML page</h1>
+    <?php
+    echo "Hello World!";
+    ?>
+</body>
+
+</html>
